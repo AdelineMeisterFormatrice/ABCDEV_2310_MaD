3bfe82c0e348ddc87aef4dc46d6fc559d761c0c3
Adeline <151820404+AdelineMeisterFormatrice@users.noreply.github.com>
2024-01-10 09:45:55 +0100

    corrriger diagramme uml

---
+++ Algo/Java/Corriger_eval_algo_condition_boucle_1/src/boucleforinfinie/App.java
@@ -0,0 +1,16 @@
+package boucleforinfinie;
+
+public class App {
+
+	public static void main(String[] args) {
+		
+		int nombre = 10;
+		
+		for(;;)
+		{
+			System.out.println(nombre);
+		}
+
+	}
+
+}
