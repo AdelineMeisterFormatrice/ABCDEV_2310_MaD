dbd549f4962c9a68ae2ae92945cb321c6dd18c96
Adeline <151820404+AdelineMeisterFormatrice@users.noreply.github.com>
2024-01-16 09:42:23 +0100

    updates

--- Objet/Java/exemple/src/exo_bouteille/App.java
+++ Objet/Java/exemple/src/exo_bouteille/App.java
@@ -4,6 +4,16 @@ public class App {
 
 	public static void main(String[] args) {
 		
+		Bouteille bouteilleParDefaut = new Bouteille();
+		Bouteille bouteilleEvian = new Bouteille("Evian", 1.5, 1.5, false);
+		
+		//boolean test1 = bouteilleParDefaut.ouvrir();
+		boolean test2 = bouteilleEvian.ouvrir();
+		//boolean test3 = bouteilleParDefaut.fermer();
+		//boolean test4 = bouteilleEvian.fermer();
+		
+		boolean test5 = bouteilleParDefaut.remplirTout();
+		boolean test6 = bouteilleEvian.remplirTout();
 
 	}
 
--- Objet/Java/exemple/src/exo_bouteille/Bouteille.java
+++ Objet/Java/exemple/src/exo_bouteille/Bouteille.java
@@ -1,5 +1,71 @@
 package exo_bouteille;
 
 public class Bouteille {
+	// declaration des attributs
+	private String nom;
+	private final double capaciteEnL;
+	private double contenanceEnL;
+	private boolean estOuverte;
 	
+	public Bouteille()
+	{
+		nom = "inconnu";
+		capaciteEnL = 1;
+		contenanceEnL = 0;
+		estOuverte = true;
+	}
+	
+	public Bouteille(String _nom,double _capaciteEnL, double _contenanceEnL, boolean _estOuverte)
+	{
+		this.nom = _nom;
+		this.capaciteEnL = _capaciteEnL;
+		this.contenanceEnL = _contenanceEnL;
+		this.estOuverte = _estOuverte;
+	}
+	
+	public boolean ouvrir()
+	{
+		if(!estOuverte)
+		{
+			this.estOuverte = true;
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+	
+	public boolean fermer()
+	{
+		if(estOuverte)
+		{
+			this.estOuverte = false;
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+	
+	public boolean remplirTout()
+	{
+		if(estOuverte)
+		{
+			if(this.contenanceEnL < this.capaciteEnL)
+			{
+				this.contenanceEnL = this.capaciteEnL;
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+		else
+		{
+			return false;
+		}
+	}
 }
