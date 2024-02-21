3f2467ab6a206430cdf4ff60023f1afffcfb9d89
Adeline <151820404+AdelineMeisterFormatrice@users.noreply.github.com>
2024-02-21 14:40:24 +0100

    up

--- Objet/Java/exemple/src/exo_recette_cuisine_v1/App.java
+++ Objet/Java/exemple/src/exo_recette_cuisine_v1/App.java
@@ -4,22 +4,19 @@ public class App {
 
 	public static void main(String[] args) {
 		
-		Plat plat1 = new Plat("Choucroute");
-		plat1.addIngredient(new Ingredient("Choucroute",500,"g"));
-		plat1.addIngredient(new Ingredient("Lard",150,"g"));
-		plat1.addIngredient(new Ingredient("Saucisse",2,"unite"));
+		Plat kougloff = new Plat("Kougloff d'Adeline");
+		kougloff.addIngredient(new Ingredient("Farine",500,"g"));
+		kougloff.addIngredient(new Ingredient("Lait",25,"cl"));
+		kougloff.addIngredient(new Ingredient("Beurre",125,"g"));
+		kougloff.addIngredient(new Ingredient("Levure",20,"g"));
+		kougloff.addIngredient(new Ingredient("Oeufs",2,"unité"));
+		kougloff.addIngredient(new Ingredient("Sucre",75,"g"));
+		kougloff.addIngredient(new Ingredient("Raisin Sec",100,"g"));
+		kougloff.addIngredient(new Ingredient("Sel",5,"g"));
 		
-		//System.out.println(plat1);
-		
-		Plat plat2 = new Plat("Choucroute au poisson");
-		plat2.addIngredient(new Ingredient("Choucroute",250,"g"));
-		plat2.addIngredient(new Ingredient("Lard",100,"g"));
-		plat2.addIngredient(new Ingredient("Saucisse",50,"g"));
-		
-		boolean test1 = plat1.equals(plat2);
-		
-		System.out.println(test1);
-
+		System.out.println(kougloff);
 	}
-
 }
+
+
+
