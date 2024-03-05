3f84fa2a2d92c7f618dce7ea3dc39f1fda7b4974
Adeline <151820404+AdelineMeisterFormatrice@users.noreply.github.com>
2024-03-05 14:14:16 +0100

    xml

--- Objet/Java/exemple/src/menu/App.java
+++ Objet/Java/exemple/src/menu/App.java
@@ -12,6 +12,22 @@ import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
 public class App {
+	
+	public static void creationListe(NodeList uneListe)
+	{
+		for(int temp = 0; temp <uneListe.getLength(); temp++)
+		{
+			Node nNode = uneListe.item(temp);
+			if(nNode.getNodeType() == Node.ELEMENT_NODE)
+			{
+				Element eElement = (Element) nNode;
+				System.out.println(nNode.getNodeName()+" id : " + eElement.getAttribute("id"));
+				System.out.println("Nom : " + eElement.getElementsByTagName("nom").item(0).getTextContent());
+				System.out.println("Prix : " + eElement.getElementsByTagName("prix").item(0).getTextContent()+" Euros");
+				System.out.println("Description : " + eElement.getElementsByTagName("description").item(0).getTextContent());
+			}
+		}
+	}
 
 	public static void main(String[] args) throws ParserConfigurationException, SAXException {
 		try {
@@ -21,21 +37,15 @@ public class App {
 			Document document = db.parse(file);
 			document.getDocumentElement().normalize();
 			System.out.println("Root Element :" + document.getDocumentElement().getNodeName());
-			NodeList nList = document.getElementsByTagName("entree");
+			NodeList listeEntree = document.getElementsByTagName("entree");
 			System.out.println("------------------------------");
-			for(int temp = 0; temp < nList.getLength(); temp++)
-			{
-				Node nNode = nList.item(temp);
-				System.out.println("\nElement courant : " + nNode.getNodeName());
-				if(nNode.getNodeType() == Node.ELEMENT_NODE)
-				{
-					Element eElement = (Element) nNode;
-					System.out.println("Entrée id : " + eElement.getAttribute("id"));
-					System.out.println("Nom : " + eElement.getElementsByTagName("nom").item(0).getTextContent());
-					System.out.println("Prix : " + eElement.getElementsByTagName("prix").item(0).getTextContent()+" Euros");
-					System.out.println("Description : " + eElement.getElementsByTagName("description").item(0).getTextContent());
-				}
-			}
+			creationListe(listeEntree);
+			NodeList listPlat = document.getElementsByTagName("plat");
+			System.out.println("------------------------------");
+			creationListe(listPlat);
+			NodeList listeDessert = document.getElementsByTagName("dessert");
+			System.out.println("------------------------------");
+			creationListe(listeDessert);
 		} catch (IOException e) {
 			System.out.println(e);
 		}
